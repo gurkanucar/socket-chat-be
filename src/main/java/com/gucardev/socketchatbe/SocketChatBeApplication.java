82ca64d821599405883af5191a82056a58d6fd4b
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-28 10:40:59 +0300

    Initial commit

---
+++ src/main/java/com/gucardev/socketchatbe/SocketChatBeApplication.java
@@ -0,0 +1,13 @@
+package com.gucardev.socketchatbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketChatBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocketChatBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gucardev/socketchatbe/SocketChatBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucardev.socketchatbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketChatBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
